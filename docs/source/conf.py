7e489e448d530cc6b96b27bea2f48d806c6cea5e
Raul P. Pelaez <raulppelaez@gmail.com>
2025-06-05 14:44:37 +0200

    Add doc files

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+
+# Project info
+project = "mean_square_displacement"
+author = "Raul P. Pelaez"
+# Get the latest git tag as release version
+try:
+    release = (
+        subprocess.check_output(
+            ["git", "describe", "--tags", "--abbrev=0"], cwd=os.path.abspath("..")
+        )
+        .decode("utf-8")
+        .strip()
+    )
+except Exception:
+    release = "unknown"
+
+
+# General config
+extensions = [
+    "breathe",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# HTML options, rtd
+html_theme = "sphinx_rtd_theme"
+
+# Breathe config
+breathe_projects = {"msd": "../doxygen/xml"}
+breathe_default_project = "msd"
+
+# Autodoc options
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "inherited-members": False,
+    "show-inheritance": False,
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. mean_square_displacement documentation master file
+
+Mean Square Displacement's documentation
+=====================================================
+
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+	     
+C++ API
+-------
+
+.. doxygenfile:: msd.hpp
+   :project: msd
+
+Python API
+----------
+
+.. autofunction:: mean_square_displacement.mean_square_displacement
